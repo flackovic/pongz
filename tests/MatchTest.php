7a33f895f99ed43c6d28a781dfdd27dd3ea25617
Latz <filackovic@gmail.com>
2018-09-11 20:10:42 +0200

    Add empty MatchTest

---
+++ tests/MatchTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MatchTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
