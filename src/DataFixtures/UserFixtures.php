d093a5cb1267f02ac748a504c0a14052d8f7a713
Latz <filackovic@gmail.com>
2018-09-13 23:27:01 +0200

    Add MatchFixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -9,11 +9,13 @@ use Faker\Factory as FakerFactory;
 
 class UserFixtures extends Fixture
 {
+    const NUM_OF_USERS = 100;
+
     public function load(ObjectManager $manager)
     {
         $faker = FakerFactory::create();
 
-        for ($i = 0; $i < 100; $i++) {
+        for ($i = 0; $i < self::NUM_OF_USERS; $i++) {
             $user = new User();
             $user->setUsername($faker->userName);
             $user->setEmail($faker->email);
